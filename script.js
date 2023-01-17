b4f94fcee9dec14a171947a6f3801aef6d24991e
sam-sol7 <samuel.solano1@outlook.com>
2023-01-17 10:03:32 -0800

    Add support for negative numbers and fix nested if-statements

--- script.js
+++ script.js
@@ -2,37 +2,45 @@ var buttons = document.getElementsByClassName("buttons");
 var equationArray = [];
 
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function () { 
+    buttons[i].onclick = function () {
         if (this.textContent == "=") {
             console.log(getResult(equationArray));
+            return;
         }
         let parsedChar = parseInt(this.textContent);
-        if (isNaN(parsedChar)) {
-            equationArray[equationArray.length] = this.textContent; 
-            // if (this.textContent == '-') {
-            //     if (isNaN(equationArray[equationArray.length - 1])) {
-                    
-            //     }
-            // } else if (this.textContent == '+') {
-
-            // } else {
-
-            // }
-        } else {
-            if (equationArray[equationArray.length-1] === undefined || isNaN(parseInt(equationArray[equationArray.length-1]))) {
-                if (equationArray.length === 0) {
-                    equationArray[equationArray.length] = parsedChar;
-                } else {
-                    equationArray[equationArray.length] = parsedChar;
-                }
-            } else {
-                equationArray[equationArray.length - 1] = equationArray[equationArray.length - 1] * 10 + parseInt(this.textContent);
+        if (!isNaN(parsedChar)) {
+            if (equationArray[equationArray.length - 1] == '--') {
+                equationArray[equationArray.length - 1] = parsedChar * -1;
+                return;
             }
+            if (equationArray[equationArray.length - 1] === undefined || isNaN(parseInt(equationArray[equationArray.length - 1]))) {
+                equationArray[equationArray.length] = parsedChar;
+                return;
+            } 
+            equationArray[equationArray.length - 1] = equationArray[equationArray.length - 1] * 10 + parseInt(this.textContent);
+
+        } else {
+            if (!isNaN(equationArray[equationArray.length - 1])) {
+                equationArray[equationArray.length] = this.textContent;
+                return;
+            } 
+            if (equationArray[equationArray.length - 1] == '--') {
+                return;
+            }
+            if (this.textContent == '-' && isNaN(equationArray[equationArray.length - 1])) {
+                if (equationArray[equationArray.length - 1] == 'x' || equationArray[equationArray.length - 1] == '÷') {
+                    equationArray[equationArray.length] = '--';
+                    return;
+                }
+            }
+            equationArray[equationArray.length - 1] = this.textContent;
         }
+        console.log(equationArray);
     };
 };
 
 function getResult(equation) {
+    console.log(equationArray);
     let total = 0;
     for (let i = 0; i < equation.length; i++) {
         let parsedChar = parseInt(equation[i]);
@@ -51,16 +59,10 @@ function getResult(equation) {
         } else {
             if (equation[i-1] === undefined) {
                 total += parsedChar;
-            } else {
-                // total = equation[i-1] + equation[i];
-                // total = parseInt(equation[i-1] + equation[i]);
-                total = total * 10 + parseInt(equation[i]);
-
-                // total = parseInt(`${equation[i-1]}${equation[i]}`);
+            } else { 
+                total = total * 10 + parseInt(equation[i]); 
             }
-        }
-
-        // console.log(equation[i]);
+        } 
     }
     return total;
 }
